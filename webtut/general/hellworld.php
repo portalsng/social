cce92a9a328f4d6843ea40231d90a8caf38f772f
PortalS ICT Solutions <e.christian@portalsng.com>
2013-06-08 23:31:21 +0100

    Added new file and folder
    Created a folder called Webtut to handle collaboration tool and a file
    called README to handle project description context

---
+++ webtut/general/hellworld.php
@@ -0,0 +1,17 @@
+<?php
+
+//A Simple Line comment
+# Another simple line comment
+/*
+* A Long comment system
+*/
+
+echo "Hello World";
+
+//You can insert html tags inside php
+echo "<br />"; 
+
+//Another way of displaying to browser
+//echo is preferred due to its popularity
+print("<strong>This is another form of Hello World</strong>");
+?>
---
+++ webtut/general/phpinfo.php
@@ -0,0 +1,6 @@
+<?php
+// @author: eChris
+// @program: A Simple script to print out the phpinfo details
+
+echo phpinfo();
+?>
